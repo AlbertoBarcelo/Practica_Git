904f5f92d396aa120a881b2bda29799bd08662f9
MRodriguezValdes <115345944+MRodriguezValdes@users.noreply.github.com>
2023-10-05 17:14:23 +0200

    Debeis crear un metodo para ordenar (#3)

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void sortArray(int[] arrayToSort){
+
+        Collection.sort(arrayToSort);
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+    }
+}
